2ebdc7235a1d36ad8d63ea9a0271c69dc0f7d240
gabrielrigolon <gabrielrigolon@gmail.com>
2022-04-22 21:07:26 -0300

    Adjusted generic method to access MongoDB

--- backend/MrMoney.Infrastructure/Data/MongoDbContext.cs
+++ backend/MrMoney.Infrastructure/Data/MongoDbContext.cs
@@ -1,28 +1,25 @@
 ﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using MrMoney.Domain.Models;
+using MrMoney.Util.Global;
 
 namespace MrMoney.Infrastructure.Data
 {
     public class MongoDbContext<T> where T: class
     {
-        public static string? ConnectionString { get; set; }
-        public static string? DatabaseName { get; set; }
-        public static bool IsSSL { get; set; }
-
         private IMongoDatabase _database { get; }
 
         public MongoDbContext()
         {
             try
             {
-                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
-                if (IsSSL)
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(DbConnectionConfiguration.ConnectionString));
+                if (DbConnectionConfiguration.IsSSL)
                 {
                     settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
                 }
                 var mongoClient = new MongoClient(settings);
-                _database = mongoClient.GetDatabase(DatabaseName);
+                _database = mongoClient.GetDatabase(DbConnectionConfiguration.DatabaseName);
             }
             catch (Exception ex)
             {
@@ -30,6 +27,6 @@ namespace MrMoney.Infrastructure.Data
             }
         }
 
-        public IMongoCollection<T> Collection { get { return _database.GetCollection<T>(nameof(T)); } }
+        public IMongoCollection<T> Collection { get { return _database.GetCollection<T>(typeof(T).Name); } }
     }
 }
